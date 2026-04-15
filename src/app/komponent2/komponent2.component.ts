32cf43211426fcd0d7a8160892e003af5823f313
gabbanasik <gb310166@student.polsl.pl>
2026-04-15 11:25:10 +0200

    zad 9

--- src/app/komponent2/komponent2.component.ts
+++ src/app/komponent2/komponent2.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-komponent2',
@@ -9,11 +9,18 @@ import { Component, Input } from '@angular/core';
 export class Komponent2Component {
 @Input() nazwaReaktora: string='';//Przyjecie nazwy od rodzica
 
-temperatura: number = 0;
+  temperaturaSignal = signal(0);
+
+  komunikatSignal = computed(() => {
+    const temp = this.temperaturaSignal(); 
+    if (temp < 16) return 'Rdzeń reaktora jest zimny, temperatura poniżej 16.';
+    if (temp <= 90) return 'Temperatura reaktora jest w normie od 16 do 90.';
+    return 'Reaktor jest w stanie krytycznym powyżej 90.';
+  });
 
   zwiekszTemperature() {
-    if (this.temperatura < 100) {
-      this.temperatura += 10;
+    if (this.temperaturaSignal() < 100) {
+      this.temperaturaSignal.update(wartosc => wartosc + 10);
     }
   }
 }
